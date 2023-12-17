8d87abecc50c9f0969b0014c018c65be88aa5f42
MuhdHanish <muhammedhanish11@gmail.com>
2023-12-17 21:08:35 +0530

    feat(pages): created home and room page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateButton from '../components/CreateButton';
+
+const Home: React.FC = () => {
+  return (
+    <div className="App w-screen h-screen flex items-center justify-center">
+      <CreateButton />
+    </div>
+  );
+}
+
+export default Home
---
+++ src/pages/Room.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Room:React.FC = () => {
+  return (
+    <div>Room</div>
+  )
+}
+
+export default Room
